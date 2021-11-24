b95339d4e66a9fe06f5efcbb7f58469e3d00b720
sanbo <sanbo.xyz@gmail.com>
2021-11-24 16:28:57 +0800

    update

---
+++ Java/src/main/java/me/hhhaiai/jitera/SdcardRun.java
@@ -0,0 +1,15 @@
+package me.hhhaiai.jitera;
+
+/**
+ * @Copyright © 2021 sanbo Inc. All rights reserved.
+ * @Description: 检测sdcard
+ * @Version: 1.0
+ * @Create: 2021/11/24 4:25 下午
+ * @author: sanbo
+ */
+public class SdcardRun {
+
+    public static void main(String[] args) {
+        
+    }
+}
